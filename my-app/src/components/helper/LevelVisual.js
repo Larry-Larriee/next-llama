83e875b82b9b0830af80f07fde64fa9a24fbe29c
Larry Le <lle114@student.hbuhsd.edu>
2024-07-22 21:39:03 -0700

    minor change
    minor change to class selector placement

--- my-app/src/components/helper/LevelVisual.js
+++ my-app/src/components/helper/LevelVisual.js
@@ -7,9 +7,9 @@ export default function LevelVisual({ levelSolution }) {
     "<head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><script src='https://cdn.tailwindcss.com'></script></head>  <style type='text/tailwindcss'>@layer utilities {.content-auto {content-visibility: auto;}}</style>";
 
   return (
-    <div className="levelSolutionUI z-5 absolute top-0 h-64 w-full rounded-md">
+    <div className="z-5 absolute top-0 h-64 w-full rounded-md">
       <iframe
-        className="h-64 w-full rounded-md bg-white"
+        className="levelSolutionUI h-64 w-full rounded-md bg-white"
         srcdoc={header + levelSolution}
       />
     </div>
