1807a6668b8e27a77c89fddc3ed4403c503a68d9
Larry Le <lle114@student.hbuhsd.edu>
2024-07-25 16:03:52 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -88,6 +88,10 @@ let imageCollection = client
 // tailwindData.level (http) the page of the level that the user is on
 // tailwindData.userSolution (string) the code that the user wrote
 app.post("/tailwindAccuracy", async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Replace with your frontend's origin
+  res.header("Access-Control-Allow-Methods", "POST"); // Allow POST method
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specific headers
+
   const { level, userSolution } = req.body;
 
   let [imageCount] = await imageCollection.find({}).toArray();
@@ -138,10 +142,6 @@ app.post("/tailwindAccuracy", async (req, res) => {
     );
 
     await browser.close();
-
-    res.header("Access-Control-Allow-Origin", "*"); // Replace with your frontend's origin
-    res.header("Access-Control-Allow-Methods", "POST"); // Allow POST method
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specific headers
 
     // make sure to return JSON and not strings because the front-end is handling JSON
     res.send({ accuracy });
