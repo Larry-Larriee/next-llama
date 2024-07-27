f99f3b75c6bbb65817ee8221df48b9ada2824c7c
Larry Le <lle114@student.hbuhsd.edu>
2024-07-27 01:11:32 -0700

    iphone, android, and ipad views
    ipad view is a work in progress at the moment.

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -69,29 +69,36 @@ export default function Page() {
           you could ever ask for.
         </p>
 
-        <section className="flex items-center justify-center gap-10 sm:flex-col">
-          <Link href="/levels/level1">
-            <p className="relative z-10 rounded-md bg-blue-500 px-8 py-4 text-xl font-bold text-white transition duration-200 ease-in-out hover:scale-105 sm:px-5 sm:py-3 sm:text-base sm:font-medium">
+        {/* note that the link in the ipad view is not clickable for the entire container */}
+        <section className="flex items-center justify-center gap-10 sm:flex-col lg:flex-row">
+          <Link
+            href="/levels/level1"
+            className="flex items-center justify-center rounded-md bg-blue-500 lg:h-16 lg:w-48"
+          >
+            <p className="relative z-10 text-xl font-bold text-white transition duration-200 ease-in-out hover:scale-105 sm:px-5 sm:py-3 sm:text-base sm:font-medium lg:px-0 lg:py-0 lg:text-center">
               Get Started
             </p>
           </Link>
 
-          <Link href="https://www.patreon.com/LarryLe">
-            <p className="relative z-10 rounded-md bg-orange-500 px-6 py-4 text-xl font-bold text-white transition duration-200 ease-in-out hover:scale-105 sm:px-5 sm:py-3 sm:text-base sm:font-medium">
+          <Link
+            href="https://www.patreon.com/LarryLe"
+            className="flex items-center justify-center rounded-md bg-orange-500 lg:h-16 lg:w-48"
+          >
+            <p className="relative z-10 text-xl font-bold text-white transition duration-200 ease-in-out hover:scale-105 sm:px-5 sm:py-3 sm:text-base sm:font-medium lg:px-0 lg:py-0 lg:text-center">
               Support my project on Patreon
             </p>
           </Link>
         </section>
       </header>
 
-      <div className="relative flex w-10/12 flex-wrap justify-between sm:gap-10">
-        <div className="primary-color-2 fat-width h-56 rounded-lg border-2 border-white sm:h-40 sm:w-full">
+      <div className="lg:space-between relative flex w-10/12 flex-wrap justify-between sm:gap-10 lg:flex-row lg:gap-0">
+        <div className="primary-color-2 fat-width h-56 rounded-lg border-2 border-white sm:h-40 sm:w-full lg:w-64">
           <p className="px-8 py-6 text-xl text-white">
             Your Tailwind Code Goes Here.
           </p>
         </div>
 
-        <div className="mt-10 flex flex-col gap-3 sm:mt-0">
+        <div className="mt-10 flex flex-col gap-3 sm:mt-0 lg:w-1/2">
           <p className="text-2xl text-white">Level Five</p>
 
           <section className="fat-width-2 flex h-80 flex-col gap-8 rounded-lg bg-yellow-400 p-5 sm:h-full sm:w-full sm:gap-5">
@@ -172,7 +179,7 @@ export default function Page() {
         </div>
 
         <p
-          className="absolute bottom-20 max-w-96 text-3xl text-white opacity-0 sm:static"
+          className="absolute bottom-20 max-w-96 text-3xl text-white opacity-0 sm:static lg:absolute lg:bottom-36 lg:w-64 lg:text-2xl"
           ref={exampOneRef}
         >
           Recreate designs. Practice and perfect tailwind.
@@ -187,24 +194,28 @@ export default function Page() {
           Level Up. Review Quickly.
         </p>
 
-        <section className="primary-color-4 ml-32 flex w-9/12 flex-col gap-6 rounded-2xl border-2 border-white pb-20 pl-10 pt-10 sm:ml-0 sm:w-10/12">
-          <p className="text-2xl text-white">Level 5</p>
+        <section className="primary-color-4 ml-32 flex w-9/12 flex-col gap-6 rounded-2xl border-2 border-white pb-20 pl-10 pt-10 sm:ml-0 sm:w-10/12 sm:items-center sm:justify-center sm:p-0 sm:pb-5 sm:pt-5">
+          <p className="text-2xl text-white sm:w-10/12 sm:text-lg">Level 5</p>
 
-          <div className="flex gap-48">
+          <div className="flex gap-48 sm:w-10/12 sm:flex-col sm:gap-12">
             <section
               className="flex flex-col gap-5 opacity-0"
               ref={exampTwoPrtTwoRef}
             >
-              <p className="text-xl text-white">Classes Your Code Used:</p>
+              <p className="text-xl text-white sm:text-lg">
+                Classes Your Code Used:
+              </p>
 
               <div>
-                <ul className="list-disc pl-8">
-                  <li className="text-md text-white">font-size (text-3xl)</li>
-                  <li className="text-md text-white">
+                <ul className="list-disc pl-8 sm:pl-4">
+                  <li className="text-base text-white">font-size (text-3xl)</li>
+                  <li className="text-base text-white">
                     background (bg-yellow-600)
                   </li>
-                  <li className="text-md text-white">width (w-full)</li>
-                  <li className="text-md text-white">max-height (max-h-64)</li>
+                  <li className="text-base text-white">width (w-full)</li>
+                  <li className="text-base text-white">
+                    max-height (max-h-64)
+                  </li>
                 </ul>
               </div>
             </section>
@@ -215,18 +226,20 @@ export default function Page() {
             >
               <div>
                 <ul>
-                  <li className="text-xl text-white">
+                  <li className="text-xl text-white sm:text-base">
                     Time taken: 9 minutes, 36 seconds
                   </li>
-                  <li className="text-xl text-white">Accuracy: 99.8%</li>
+                  <li className="text-xl text-white sm:text-base">
+                    Accuracy: 99.8%
+                  </li>
                 </ul>
               </div>
 
-              <div className="flex flex-col gap-5">
-                <p className="w-56 cursor-default rounded-lg border-2 border-white bg-blue-500 px-5 py-4 text-center text-xl font-bold text-white transition duration-200 ease-in-out hover:scale-105">
+              <div className="flex flex-col gap-5 sm:flex-row sm:justify-between sm:gap-0">
+                <p className="w-56 cursor-default rounded-lg border-2 border-white bg-blue-500 px-5 py-4 text-center text-xl font-bold text-white transition duration-200 ease-in-out hover:scale-105 sm:w-28 sm:px-3 sm:py-2 sm:text-base sm:font-medium">
                   Your Code
                 </p>
-                <p className="w-56 cursor-default rounded-lg border-2 border-white bg-orange-500 px-5 py-4 text-center text-xl font-bold text-white transition duration-200 ease-in-out hover:scale-105">
+                <p className="w-56 cursor-default rounded-lg border-2 border-white bg-orange-500 px-5 py-4 text-center text-xl font-bold text-white transition duration-200 ease-in-out hover:scale-105 sm:w-28 sm:px-3 sm:py-2 sm:text-base sm:font-medium">
                   Sample Solution
                 </p>
               </div>
@@ -239,19 +252,19 @@ export default function Page() {
       <div className="relative bottom-16 flex w-full flex-col items-center justify-center gap-24">
         <section className="radial-gradient-absolute-2 radial-gradient-height radial-gradient-width" />
 
-        <section className="flex flex-col gap-5">
-          <p className="text-center text-5xl text-white">
+        <section className="flex flex-col gap-5 sm:w-10/12 md:items-center">
+          <p className="text-center text-5xl text-white sm:text-3xl">
             Documentation on file
           </p>
-          <p className="text-center text-2xl text-white">
+          <p className="text-center text-2xl text-white sm:text-xl md:w-10/12">
             so there&apos;s no need to constantly switch tabs
           </p>
         </section>
 
-        <section className="flex w-10/12 justify-between">
-          <div className="flex w-5/12 flex-col gap-5">
+        <section className="flex w-10/12 justify-between sm:flex-col sm:gap-10 md:gap-16">
+          <div className="flex w-full flex-col gap-5">
             <section
-              className="primary-color-2 h-72 w-full rounded-xl border-2 border-white pl-10 pt-10 opacity-0"
+              className="primary-color-2 h-72 w-full rounded-xl border-2 border-white pl-10 pt-10 opacity-0 sm:h-48 sm:pl-8 sm:pt-8 md:pl-5 md:pt-5"
               ref={exampThrPrtOneRef}
             >
               <p className="h-48 text-xl text-white">
@@ -272,20 +285,22 @@ export default function Page() {
           </div>
 
           <div
-            className="h-72 w-6/12 rounded-xl bg-gray-300 opacity-0"
+            className="h-72 w-6/12 rounded-xl bg-gray-300 opacity-0 sm:h-48 sm:w-full"
             ref={exampThrPrtTwoRef}
           >
-            <p className="p-10 text-xl text-black">Hello World</p>
+            <p className="pl-10 pt-10 text-xl text-black sm:pl-8 sm:pt-8">
+              Hello World
+            </p>
           </div>
         </section>
       </div>
 
-      <div className="mb-5 flex w-10/12 justify-between">
+      <div className="mb-5 flex w-10/12 justify-between sm:gap-1 md:gap-2">
         <p className="text-lg text-white">
           ©2024 Larry Le All Rights Reserved
         </p>
 
-        <section className="flex gap-10">
+        <section className="flex gap-10 sm:flex-col sm:gap-3">
           <Link href="/levels/level1">
             <p className="text-lg text-white transition duration-200 ease-in-out hover:-translate-y-1">
               Get Started
